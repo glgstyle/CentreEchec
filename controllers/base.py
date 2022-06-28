6744cf01c5118c31434c2540fd50bfda59479f24
glgstyle <gleguenno@gmail.com>
2022-06-28 14:16:54 +0200

    Move display function to view, try to shuffle list of players if they have ever played together(doesn't work correctly)

--- controllers/base.py
+++ controllers/base.py
@@ -4,6 +4,7 @@ from time import strptime
 from typing import List
 from datetime import datetime
 import random
+from models import tournament
 from models.player import Player
 from models.tournament import Tournament
 from models.match import Match
@@ -19,7 +20,7 @@ class Controller:
         self.players = []
         self.tournament = Tournament
         self.match = Match()
-        self.round = []
+        #self.round = []
 
     def make_a_tournament_team(self):
         """Add players until players list = 8, return the list of players."""
@@ -31,57 +32,14 @@ class Controller:
     
     def create_a_tournament(self):
         """Set up a new tournament"""
-        print("--------Création d'un tournoi--------")
-        self.tournament.name = input("Veuillez créer un nom pour ce tournoi : ")
-        self.tournament.date = input("Veuillez saisir la date du tournoi(jj/mm/aaaa) :")
-        # As long as the date format is incorrect request the date again, then reformat the date before inserting it 
-        while True:
-            try: 
-                self.tournament.date = datetime.strptime(self.tournament.date, "%d/%m/%Y")
-                break
-            except ValueError:
-                print("La date n'est pas au bon format, Veuillez recommencer")
-                self.tournament.date = input("Veuillez saisir la date du tournoi(jj/mm/aaaa) :")
-        self.tournament.place = input("Veuillez saisir le lieu du tournoi :")
-        try:
-            self.tournament.numbers_of_turns = int(input(
-            "Veuillez saisir le nombre de tours (appuyez sur entrée = par défaut 4) ") or "4")
-        except ValueError:
-            self.tournament.numbers_of_turns = 4
-            print(f"Le nombre de tours est incorrect, utilisation de la valeur par défaut : {self.tournament.numbers_of_turns} ")
+        View.display_create_a_tournament(self.tournament)
         self.make_a_tournament_team()
-        
-    def display_team(self, team):
-        """Print the competitor list in shuffle order(already done in function make_players_pairs)"""
-        for player in team:
-            print(player.name, player.firstname)
 
     def sort_players_by_rank(self):
-        """Print the list of players sorted by rank, return the sorted list"""
+        """Sort the list of players sorted by rank, return the sorted list"""
         sorted_by_rank = sorted(self.players, key=lambda player: player.rank)
-        print(sorted_by_rank)
         return sorted_by_rank
 
-    """def make_players_pairs(self):
-        #Make teams of two, return the list of players.
-        # while there is players to put in teams and teams to create, return a list of random players
-        random.shuffle(self.players)
-        # each pair of player in a variable
-        first_team = self.players[0:2]
-        second_team = self.players[2:4]
-        third_team = self.players[4:6]
-        fourth_team = self.players[6:8]
-        # the list of pairs of players
-        self.list_of_teams = [first_team , second_team , third_team , fourth_team]
-        print(f"Les bibômes sont les suivants :\n\nEquipe A :")
-        self.display_team(self.list_of_teams[0])
-        print(f"\nEquipe B :")
-        self.display_team(self.list_of_teams[1])
-        print(f"\nEquipe C :")
-        self.display_team(self.list_of_teams[2])
-        print(f"\nEquipe D :")
-        self.display_team(self.list_of_teams[3])
-        return self.players"""
     def make_players_pairs(self):
         """Divide sorted players in two half, the best player of upper half play against the best player of lower half etc..."""
         sorted_players_by_rank = self.sort_players_by_rank()
@@ -89,8 +47,6 @@ class Controller:
         half = int(half)
         upper_half = sorted_players_by_rank[0:half] 
         lower_half = sorted_players_by_rank[half:len(sorted_players_by_rank)]
-        print(upper_half)
-        print(lower_half)
         first_team = upper_half[0], lower_half[0]
         second_team = upper_half[1], lower_half[1]
         third_team = upper_half[2], lower_half[2]
@@ -98,14 +54,15 @@ class Controller:
         # the list of pairs of players
         self.list_of_teams = [first_team , second_team , third_team , fourth_team]
         print(f"Les bibômes sont les suivants :\n\nEquipe A :")
-        self.display_team(self.list_of_teams[0])
+        View.display_team(self.list_of_teams[0])
         print(f"\nEquipe B :")
-        self.display_team(self.list_of_teams[1])
+        View.display_team(self.list_of_teams[1])
         print(f"\nEquipe C :")
-        self.display_team(self.list_of_teams[2])
+        View.display_team(self.list_of_teams[2])
         print(f"\nEquipe D :")
-        self.display_team(self.list_of_teams[3])
-
+        View.display_team(self.list_of_teams[3])
+        return self.list_of_teams
+                    
     def make_players_pairs_by_score_or_rank(self):
         """Make players pairs by score or rank after the first round"""
         sorted_by_score_or_rank = self.sort_players_by_score_then_rank()
@@ -115,14 +72,36 @@ class Controller:
         team4 = sorted_by_score_or_rank[6:8]
         # the list of pairs of players
         self.teams = [team1 , team2 , team3 , team4]
+        for round in self.rounds:
+            print(f"ici------------{round.players}")
+            for pair in round.players:
+                    while True:
+                        try:
+                            pair in self.teams or reversed(pair) in self.teams or pair in self.list_of_teams or reversed(pair) in self.list_of_teams
+                            #print ("The original list is : " + str(sorted_by_score_or_rank)) 
+                            for i in range(len(sorted_by_score_or_rank)-1, 0, -1): 
+                                j = random.randint(0, i + 1) 
+                                sorted_by_score_or_rank[i], sorted_by_score_or_rank[j] = sorted_by_score_or_rank[j], sorted_by_score_or_rank[i] 
+                            #print ("The shuffled list is : " +  str(sorted_by_score_or_rank))
+                        except: 
+                            #print("pas de doublon")
+                            team1 = sorted_by_score_or_rank[0:2]
+                            team2 = sorted_by_score_or_rank[2:4]
+                            team3 = sorted_by_score_or_rank[4:6]
+                            team4 = sorted_by_score_or_rank[6:8]
+                            self.teams = [team1, team2, team3, team4]
+                            break
+        print(f"self.teams : {self.teams}")
+        print(f"self.list_of_teams : {self.list_of_teams}")
         print(f"Les bibômes sont les suivants :\n\nEquipe A :")
-        self.display_team(self.teams[0])
+        View.display_team(self.teams[0])
         print(f"\nEquipe B :")
-        self.display_team(self.teams[1])
+        View.display_team(self.teams[1])
         print(f"\nEquipe C :")
-        self.display_team(self.teams[2])
+        View.display_team(self.teams[2])
         print(f"\nEquipe D :")
-        self.display_team(self.teams[3])
+        View.display_team(self.teams[3])
+        return self.teams
 
     def sort_players_by_score_then_rank(self):
         """Sorted the list of players by score first and if score is equal, sort them by rank"""
@@ -133,37 +112,15 @@ class Controller:
     def match_record(self):
         """Record the players in a match and return them"""
         players_in_match = []
-        for player in self.players:
-            #players_in_match.append([player.firstname , player.name, player.points])
+        sorted_by_score_or_rank = self.sort_players_by_score_then_rank()
+        for player in sorted_by_score_or_rank:
             players_in_match.append(player)
         return players_in_match
 
     def results_of_match(self):
         """Input the players results in the list, insert the pair of players and their reuslts in match """
         self.match.player_match_result = self.input_results()
-        """for player in self.match.player_match_result:
-            print(f"//les points {player.points}")
-        print(f"//////player match result{self.match.player_match_result}")"""
         self.match.pair_of_players = self.match_record()
-
-    def is_the_match_finished(self):
-        """Ask to press enter when the match is finished and record the end time"""
-        input("Appuyez sur entrée lorsque le match est terminé.")
-        return self.end_time()
-
-    def start_time(self):
-        """Define the time when start the match."""
-        now = datetime.now()
-        current_time = now.strftime("%d/%m/%Y %H:%M:%S")
-        print(f"Heure de démarrage : {current_time}")
-        return current_time
-    
-    def end_time(self):
-        """Define the time when end the match."""
-        now = datetime.now()
-        actual_time = now.strftime("%d/%m/%Y %H:%M:%S")
-        print(f"Heure de fin : {actual_time}")
-        return actual_time
 
     def name_a_round(self):
         """Copy the number of turns in tournament to get an iteration of round, then return the name of rounds."""
@@ -184,24 +141,21 @@ class Controller:
             #***
             self.make_players_pairs()
             input("Appuyez sur entrée pour démarrer le match")
-            round.start_time = self.start_time()
-            round.end_time = self.is_the_match_finished()
+            round.start_time = View.start_time(round)
+            round.end_time = View.is_the_match_finished(round)
             round.results = self.results_of_match()
-            round.players = self.match.pair_of_players
+            round.players = self.list_of_teams
             self.update_the_score()
             self.rounds.append(round)
         for i in list_name_round[1:len(list_name_round)]:
             round = Round()
             round.name = i
             print(i)
-            #***
-            #self.sort_players_by_score_then_rank()
-            self.make_players_pairs_by_score_or_rank()
+            round.players = self.make_players_pairs_by_score_or_rank()
             input("Appuyez sur entrée pour démarrer le match")
-            round.start_time = self.start_time()
-            round.end_time = self.is_the_match_finished()
+            round.start_time = View.start_time(round)
+            round.end_time = View.is_the_match_finished(round)
             round.results = self.results_of_match()
-            round.players = self.match.pair_of_players
             self.update_the_score()
             self.rounds.append(round)
         View.display_infos_rounds(self.rounds)   
@@ -224,10 +178,8 @@ class Controller:
                         player.points = [*player.points , points]
                         break
                     except ValueError:
-                        print(f"({points}) n'est pas un score valide veuillez rentrer un chiffre ou un nombre ")  
-        print("*****Match terminé, vosici les scores :*****")
-        for player in self.players:
-            print(player.name, player.firstname, player.points)
+                        print(f"({points}) n'est pas un score valide veuillez rentrer un chiffre ou un nombre ") 
+        View.display_match_score(self) 
         return self.players
        
     def update_the_score(self):
@@ -241,4 +193,3 @@ class Controller:
                 player.score += points
             #display the player with his score
             print(f"{player.firstname} {player.name} :{player.score} points")
-
--- views/base.py
+++ views/base.py
@@ -4,6 +4,7 @@
 from models import player
 from models.player import Player
 from models.round import Round
+from datetime import datetime
 
 class View:
     '''Chess game.'''
@@ -11,12 +12,70 @@ class View:
     def __init__(self):
         '''Define the view.'''
 
+    def display_create_a_tournament(tournament):
+        """Display the datas to enter for the tournament."""
+        print("--------Création d'un tournoi--------")
+        tournament.name = input("Veuillez créer un nom pour ce tournoi : ")
+        tournament.date = input("Veuillez saisir la date du tournoi(jj/mm/aaaa) :")
+        # As long as the date format is incorrect request the date again, then reformat the date before inserting it 
+        while True:
+            try: 
+                tournament.date = datetime.strptime(tournament.date, "%d/%m/%Y")
+                break
+            except ValueError:
+                print("La date n'est pas au bon format, Veuillez recommencer")
+                tournament.date = input("Veuillez saisir la date du tournoi(jj/mm/aaaa) :")
+        tournament.place = input("Veuillez saisir le lieu du tournoi :")
+        try:
+            tournament.numbers_of_turns = int(input(
+            "Veuillez saisir le nombre de tours (appuyez sur entrée = par défaut 4) ") or "4")
+        except ValueError:
+            tournament.numbers_of_turns = 4
+            print(f"Le nombre de tours est incorrect, utilisation de la valeur par défaut : {tournament.numbers_of_turns} ")
+    
+    def display_team(team):
+        """Print the competitor list in shuffle order(already done in function make_players_pairs)"""
+        for player in team:
+            print(player.name, player.firstname)
+
+    def start_time(round):
+        """Define the time when start the match."""
+        now = datetime.now()
+        round.start_time = now.strftime("%d/%m/%Y %H:%M:%S")
+        print(f"Heure de démarrage : {round.start_time}")
+        return round.start_time
+    
+    def end_time(round):
+        """Define the time when end the match."""
+        now = datetime.now()
+        round.end_time = now.strftime("%d/%m/%Y %H:%M:%S")
+        print(f"Heure de fin : {round.end_time}")
+        return round.end_time
+    
+    def is_the_match_finished(round):
+        """Ask to press enter when the match is finished and record the end time"""
+        input("Appuyez sur entrée lorsque le match est terminé.")
+        return View.end_time(round)
+
     def display_infos_rounds(rounds):
+        """Dispaly the round informations at the end of tournament"""
         #print(round.start_time)
         print(f"**********Voici les infos des rounds{rounds}")
         for round in rounds:
             print(f"Dans le round : {round.name}")
             print(f"qui s'est joué le {round.start_time}")
             print(f"et s'est terminé le {round.end_time}")
-            print(f"avec les joueurs suivants : {round.players}")
-            print(f"Voici les résultats : {round.results}")
+            #print(f"qui a opposé {round.players}")
+            #for round in rounds:
+        #i=0
+        #while i < len(round.players):
+            print(f"qui a opposé :")
+            for p in round.players:
+                print(f"{p[0]} - contre - {p[1]}")
+            #i+=2
+
+    def display_match_score(round):
+        print("*****Match terminé, voici les scores :*****")
+        for player in round.players:
+            print(player.name, player.firstname, player.points)
+
