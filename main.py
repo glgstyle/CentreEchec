35c629961317f5524b1d5880d1eea675e42bd182
glgstyle <gleguenno@gmail.com>
2022-07-06 16:15:56 +0200

    Try Qt GUI

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 '''Entry point'''
 
+
 from controllers.base import Controller
-from models.player import Player
+#from models.player import Player
+from ControlPanelCentreEchec.widget import Widget
+from PySide2.QtWidgets import QApplication, QWidget, QPushButton
+import sys
+
+
 
 def main():
     controller = Controller()
     controller.start_a_tournament()
 
-if __name__ == "__main__":
-    main()
 
+if __name__ == "__main__":
+    #Widget.window()
+    main()
